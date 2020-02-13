639e9df325529d25c5571798ee1265614558ad30
angel <angelsb94@gmail.com>
2020-02-12 20:53:00 -0600

    agregando boton de agregar contacto

--- src/components/Directory.jsx
+++ src/components/Directory.jsx
@@ -5,24 +5,48 @@ import ContactCard from './ContactCard';
 import contactsData from '../utils/contactsData'
 
 class Directory extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             contacts: contactsData,
+            isToggleOn: false,
         }
+        this.handleClick = this.handleClick.bind(this);
     }
-
+    handleClick(){
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+          }));
+    }
 
     render() {
         const card = this.state.contacts.map((contact, idx) => 
         <ContactCard info={contact}  key={idx}/> );
 
 
+        console.log(this.state.isToggleOn)
+        if (this.state.isToggleOn === true){
+            console.log("Este boton agregara un contacto al directorio")
+            this.state.isToggleOn = false
+        }else{
+            console.log("No hay nada")
+        }
+
+
         return (
             <div className='container'>
                 <div className="row">
-                    <div className="col s12">
+                    <div className="col s9">
                         {card}
+                    </div>
+                    <div className="col s3">
+                        <a 
+                        className="waves-effect waves-light btn-large"
+                        onClick={this.handleClick}
+                        >
+                            <i className="material-icons right">add</i>
+                            agregar
+                        </a>
                     </div>
                 </div>
             </div>
